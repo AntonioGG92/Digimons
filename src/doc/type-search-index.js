903767469ebb2f6cb0a07f7f2578da99c5914903
Antonio <antonio92guerrerogarcia@gmail.com>
2024-05-31 12:41:10 +0200

    Javadoc

---
+++ src/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Batalla_Digital"},{"p":"<Unnamed>","l":"Digimon"},{"p":"<Unnamed>","l":"Domador"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
